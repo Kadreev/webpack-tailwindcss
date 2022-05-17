91422194fdb8dc3ccda16dad72d82116b611a349
Kadreev <kadreev@gmail.com>
2022-05-17 04:45:13 +0300

    update 5 files

--- src/js/app.js
+++ src/js/app.js
@@ -1,131 +1,110 @@
 import '../css/app.css';
 import Alpine from 'alpinejs'
 import axios from 'axios';
+window.axios = require('axios');
+
 window.Alpine = Alpine
+
 Alpine.directive('uppercase', el => {
     el.textContent = el.textContent.toUpperCase()
 })
 
-// Alpine.data('dropdown', () => ({
-//     open: false,
-
-//     toggle() {
-//         this.open = !this.open
-//     }
-// }))
-
-// window.Alpine.store('store', {
-//     items: [],
-// }, true)
-// window.myComponent = function() {
-//     return {
-//         itemIds() {
-//             return this.$store['store'].items
-//         },
-//         getItems() {
-//             let vm = this;
-//             return axios(config).then((response) => {
-//                 vm.$store['store'].items = response.data.data.product.title;
-//                 console.log(vm);
-//             }).catch((error) => {
-//                 console.log(error)
-//             })
-//         },
-//     }
-// }
-
-
-Alpine.data('main', () => ({
-    light: true,
-    medium: false,
-    dark: false,
-    decaf: false,
-
-    setEnter(roast) {
-        switch (roast) {
-            case 'light':
-                this.light = true
-                this.medium = false
-                this.dark = false
-                this.decaf = false
-                break;
-            case 'medium':
-                this.medium = true
-                this.light = false
-                this.dark = false
-                this.decaf = false
-                break;
-            case 'dark':
-                this.dark = true
-                this.light = false
-                this.medium = false
-                this.decaf = false
-                break;
-            case 'decaf':
-                this.decaf = true
-                this.light = false
-                this.medium = false
-                this.dark = false
-                break;
-        }
-
-    }
-}))
-
-let data = JSON.stringify({
-    query: `query FetchProductSample {
-  product(handle: "main-roast") {
-    id
-    title
-    variants(first: 15) {
-      edges {
-        node {
-          id
-          title
-          sku
-          priceV2 {
-            amount
-          }
-          image {
-            url(transform: {maxWidth: 300})
-            altText
-          }
-          selectedOptions {
-            name
-            value
+const Products = () => {
+    const data = JSON.stringify({
+        query: `query FetchProductSample {
+      product(handle: "main-roast") {
+        id
+        title
+        variants(first: 15) {
+          edges {
+            node {
+              id
+              title
+              sku
+              priceV2 {
+                amount
+              }
+              image {
+                url(transform: {maxWidth: 300})
+                altText
+              }
+              selectedOptions {
+                name
+                value
+              }
+            }
           }
         }
       }
-    }
-  }
-}`,
-    variables: {}
-});
-
-let config = {
-    method: 'post',
-    url: 'https://lifeboostcoffee.myshopify.com/api/2022-04/graphql.json',
-    headers: {
-        'X-Shopify-Storefront-Access-Token': 'f81f808b4e00de733d0e8195d5e0c6a9',
-        'Content-Type': 'application/json'
-    },
-    data: data
-};
-
-axios(config)
-    .then(function(response) {
-        console.log(response.data.data.product.variants.edges);
-        Alpine.store('productStore', {
-            "products": response.data.data.product.variants.edges
-        })
-    })
-    .catch(function(error) {
-        console.log(error);
+    }`,
+        variables: {}
     });
+
+    const config = {
+        method: 'post',
+        url: 'https://lifeboostcoffee.myshopify.com/api/2022-04/graphql.json',
+        headers: {
+            'X-Shopify-Storefront-Access-Token': 'f81f808b4e00de733d0e8195d5e0c6a9',
+            'Content-Type': 'application/json'
+        },
+        data: data
+    };
+    return {
+        itemIds() {
+            return this.$store.productStore.products
+        },
+        async getItems() {
+            let vm = this;
+            return axios(config).then((response) => {
+                Alpine.store('productStore', {
+                    products: response.data.data.product.variants.edges,
+                    async getVar(roast, type) {
+                        return this.products.filter(product => {
+                            return product.node.title.toLowerCase().includes(type.toLowerCase()) && product.node.title.toLowerCase().includes(roast.toLowerCase())
+
+                        })
+
+                    }
+                })
+                console.log(vm);
+            }).catch((error) => {
+                console.log(error)
+            })
+        },
+    }
+}
+console.log(Products().getItems());
+
+Alpine.data('roast', () => ({
+    roast: 'light',
+    setRoast(roast) {
+        this.roast = roast
+    }
+}))
+
+
+
+Alpine.data('type', () => ({
+    type: 'beans',
+    setType(type) {
+        this.type = type
+    }
+}))
+
+Alpine.store('productStore', {
+    products: {},
+
+    // axios(config).then(function(response) {
+    //         this.products = response.data.data.product.variants.edges
+    //     })
+    //     .catch(function(error) {
+    //         console.log(error);
+    //     });
+
+})
 
 
 Alpine.start()
-
 
 
 
